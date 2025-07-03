de939cd762c2a00ebf6375691206717a5d7f4622
AhmmedSalama <a7medsamirr4@gmail.com>
2025-07-03 15:22:07 +0300

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ function NavBar() {
       <div className="w-full flex justify-between items-center lg:block">
         <a>
           <img
-            src="imgs/logo.png"
+            src="imgs/logo.webp"
             className="h-10"
             alt="Ahmed logo"
             loading="lazy"
